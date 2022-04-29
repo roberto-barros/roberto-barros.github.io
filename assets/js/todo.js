1107fd7f21cfb5909ad6cd3966bb1a100d4fd63b
Roberto Barros <roberto_barros@live.com>
2022-04-29 11:42:52 -0300

    Aula 3

---
+++ assets/js/todo.js
@@ -0,0 +1,55 @@
+const db = [
+    {
+        id: 1,
+        title: 'Concluir App Conexão Arte',
+        steps: [
+            { step: 'Ajustar textos' },
+            { step: 'Trocar imagens para imagens públicas' },
+            { step: 'Públicar no Expo' },
+            { step: 'Publicar no Expo Store Connect' },
+        ],
+        done: false,
+        dueDate: '2022-05-06',
+        reminder: '2022-05-02 10:00',
+    },
+
+    {
+        id: 2,
+        title: 'Aula 4 Fiap - Avanade',
+        steps: [
+            { step: 'Atributos Globais' }, { step: 'Estrutura CSS' }],
+        done: false,
+        dueDate: '2022-05-06',
+        reminder: '2022-05-02 10:00',
+    },
+];
+
+// console.log(db[0].title);
+
+const form = document.querySelector('#addNewTask');
+const newTask = document.querySelector('#inputTxtNewTask');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+newTask.addEventListener('keyup', (e) => {
+    console.log('keyup');
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.key == 'Enter') {
+        if (!newTask.value) {
+            alert('Digite uma nova tarefa, para adicionar');
+        } else {
+            alert(newTask.value);
+
+            const d = new Date();
+            const today = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+
+            db.push({ id: Number(db.length) + 1, title: newTask.value, done: false, dueDate: today });
+            newTask.value = '';
+            console.log(db);
+        }
+    }
+});
+
---
+++ assets/js/utils/domFunctions.js
@@ -0,0 +1,19 @@
+export function addTask() {
+    alert("Cadastrando uma nova tarefa");
+}
+
+export function removeTask() {
+    alert("Removendo uma nova tarefa");
+}
+
+export function updateTask() {
+    alert("Alterando uma tarefa");
+}
+
+export function getAllTask() {
+    alert("LIstando todas as tarefas");
+}
+
+export function getTaskById() {
+    alert("Exibindo uma tarefa");
+}
